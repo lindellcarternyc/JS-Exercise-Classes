57a9f18d90efa4b8a2c81050dd5c9bd990e44821
lindellcarternyc <lindellcarternyc@gmail.com>
2021-01-26 14:28:23 -0500

    complete task 1

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 */
 
 // EXAMPLE SOLUTION CODE:
+// eslint-disable-next-line no-unused-vars
 class Airplane {
     constructor(name) {
       this.name = name;
@@ -22,28 +23,46 @@ class Airplane {
     }
   }
   
-  /*
-  // 👇 COMPLETE YOUR WORK BELOW 👇
-  // 👇 COMPLETE YOUR WORK BELOW 👇
-  // 👇 COMPLETE YOUR WORK BELOW 👇
-  */
-  
-  /*
-    TASK 1
-      - Write a Person class whose constructor initializes `name` and `age` from arguments.
-      - All instances of Person should also initialize with an empty `stomach` array.
-      - Give instances of Person the ability to `.eat("someFood")`:
-          + When eating an edible, it should be pushed into the `stomach`.
-          + The `eat` method should have no effect if there are 10 items in the `stomach`.
-      - Give instances of Person the ability to `.poop()`:
-          + When an instance poops, its `stomach` should empty.
-      - Give instances of Person a method `.toString()`:
-          + It should return a string with `name` and `age`. Example: "Mary, 50"
-  */
-  
- class Person {
-    
+/*
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
+// 👇 COMPLETE YOUR WORK BELOW 👇
+*/
+
+/*
+  TASK 1
+    - Write a Person class whose constructor initializes `name` and `age` from arguments.
+    - All instances of Person should also initialize with an empty `stomach` array.
+    - Give instances of Person the ability to `.eat("someFood")`:
+        + When eating an edible, it should be pushed into the `stomach`.
+        + The `eat` method should have no effect if there are 10 items in the `stomach`.
+    - Give instances of Person the ability to `.poop()`:
+        + When an instance poops, its `stomach` should empty.
+    - Give instances of Person a method `.toString()`:
+        + It should return a string with `name` and `age`. Example: "Mary, 50"
+*/
+
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+    this.stomach = []
   }
+
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      return this.stomach.push(someFood)
+    }
+  }
+
+  poop() {
+    this.stomach = []
+  }
+
+  toString() {
+    return `${this.name}, ${this.age}`
+  }
+}
   
   /*
     TASK 2
